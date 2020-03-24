f1774f512ba2043346a830292b9123dae64eef44
Zhaolin Li <zhaolin.li@wur.nl>
2020-03-24 11:22:43 +0100

    maze works

--- Localization with KF/main.py
+++ Localization with KF/main.py
@@ -1,19 +1,26 @@
 # from motion_control import Motion
 # from sensor import Sensor
-from map import MapClass
+import maze
 # from track_pose import Pose
 # from visualization import *
 import pygame
 
 def main():
 	run = True
-	game_map = MapClass()
-	game_map.initilize_screen()
-	game_map.draw_borders()
-	game_map.draw_walls()
+	pygame.init()
+	win = pygame.display.set_mode((maze.map_width, maze.map_height))
+	pygame.display.set_caption('Simulator')
+	win.fill((173, 216, 230))
 	# controller = Motion(game_map)
 	while run:
-		game_map.update_screen(x=50, y=50, angle=100, radius=20, deltaT=0)
-		# v,w = controller.update_speed()
-	run = False
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				run = False
+		maze.draw_borders(win)
+		maze.draw_walls(win)
+
+		maze.update_screen(win, x=50, y=50, angle=100, radius=20)
+
+	# 	# v,w = controller.update_speed()
+
 main()
---
+++ Localization with KF/maze.py
@@ -0,0 +1,59 @@
+'''
+
+Initilizing maze and point-based features
+No walls, no collision with features
+Known correspondence which means all features have unique labels
+screen visualization
+'''
+import pygame
+from visualization import *
+from shapely.geometry import *
+
+
+#### Variables ####
+map_width = 500
+map_height = 500
+borders = [Point(0, 0), Point(0, map_width), Point(map_height, map_width), Point(map_height, 0)]
+wall1 = [Point(0, 150), Point(350, 150), Point(350, 400), Point (250, 400)]
+wall2 = [Point(150, map_height), Point(150, 300), Point (280, 300)]
+maze = [borders, wall1, wall2]
+features = [Point(0, 0), Point(0, map_width), Point(map_height, map_width), Point(map_height, 0),
+            Point(0, 150), Point(350, 150), Point(350, 300), Point (250, 300),
+            Point(150, map_height), Point(150, 250), Point (300, 250)]
+
+
+
+
+
+def draw_borders(win):
+    for i in range(len(borders) - 1):
+        pygame.draw.line(win, (255, 0, 0), (int(borders[i].x), int(borders[i].y)),
+                         (int(borders[i + 1].x), int(borders[i + 1].y)), 5)
+
+    pygame.draw.line(win, (255, 0, 0), (int(borders[3].x), int(borders[3].y)),
+                    (int(borders[0].x), int(borders[0].y)), 5)
+
+def draw_walls(win):
+    #### Wall1 ####
+    for i in range(len(wall1)-1):
+        pygame.draw.line(win, (255, 0, 0), (int(wall1[i].x), int(wall1[i].y)),
+                         (int(wall1[i + 1].x), int(wall1[i + 1].y)), 2)
+    #### Wall2 ####
+    for i in range(len(wall2)-1):
+        pygame.draw.line(win, (255, 0, 0), (int(wall2[i].x), int(wall2[i].y)),
+                         (int(wall2[i + 1].x), int(wall2[i + 1].y)), 2)
+
+
+def update_screen(win, x, y, angle, radius, deltaT=5):
+    ### Draw robot ###
+    pygame.draw.circle(win, (0, 255, 0), (int(x), int(y)), radius)
+    pygame.draw.line(win, (255, 255, 0), (int(x), int(y)),
+                     (x + radius * -np.cos(angle), (y + radius * np.sin(angle))),
+                     int(radius / 10))
+    pygame.display.update()
+    pygame.time.delay(deltaT)
+
+def quit_screen(): #TOBE TEST
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
